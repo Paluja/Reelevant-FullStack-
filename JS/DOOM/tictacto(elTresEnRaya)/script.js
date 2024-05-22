f4e2244ec8599f878f8cb8272c3e2ff6e9b84593
paluja <jtp1006@hotmail.com>
2024-05-22 19:26:43 +0200

    tres en raya

--- JS/DOOM/tictacto(elTresEnRaya)/script.js
+++ JS/DOOM/tictacto(elTresEnRaya)/script.js
@@ -1,1 +1,40 @@
-const 
+const squares = document.querySelectorAll('td');
+const restart = document.querySelector('button');
+// const rows = document.querySelectorAll()
+const board = [];
+let turn = 0;
+
+
+restart.addEventListener('click', () => {
+    squares.forEach(
+        (obj) => {
+            obj.textContent = '';
+            obj.style.pointerEvents = 'auto';
+    }
+    )}
+)
+
+const handleClick = (e, i) => {
+    const box = e.target;
+    let content = 'oooo';
+    
+    turn ++;
+    if (turn % 2)
+        content = 'X';
+    else
+        content = 'O';
+    box.textContent = content;    
+    board[i] = content;
+    box.style.pointerEvents = 'none';
+};
+
+
+// const its_win
+
+
+
+squares.forEach(
+    (obj, i) => {
+        obj.addEventListener('click', (e) => handleClick(e,i))
+    }
+);
