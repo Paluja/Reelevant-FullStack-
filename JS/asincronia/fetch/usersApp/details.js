01b0a1319a1308ae0667229aedfa2f4fbf6e26d5
paluja <jtp1006@hotmail.com>
2024-05-31 14:31:41 +0200

    si

---
+++ JS/asincronia/fetch/usersApp/details.js
@@ -0,0 +1,111 @@
+const container = document.querySelector('#results');
+const options = {
+	method: 'GET',
+	headers: {
+		'app-id': '6655bc199d926479175f28c2'
+	}
+};
+document.addEventListener('DOMContentLoaded', async () =>{
+    const params = new URLSearchParams(window.location.search);
+    const userId = params.get('id');
+    
+    if (userId){
+        try {
+            //lo de la pregunta por asi decirlo seria como para filtrar los datos al traerlos
+            //sin pregunta seria como hacer un nuevo fetch
+            const response = await fetch(`https://dummyapi.io/data/v1/user/${userId}`, options);
+            if(!response.ok)
+                throw new Error("Error al obtener los datos");
+            const result = await response.json();
+            console.log(result);
+            showDetails(result);
+        } catch (error) {
+            console.error(error);
+        }
+        getPosts(userId);
+    }
+});
+
+
+const getPosts = async (userId)  =>{
+    try{
+        const response = await fetch(`https://dummyapi.io/data/v1/user/${userId}/post`, options); 
+        if(!response.ok)
+            throw new Error("Error al obtener los datos");
+        const result = await response.json();
+        console.log(result.data);
+        showPosts(result.data);
+    } catch {
+        console.error(error);
+    }
+}
+
+const showDetails = (result) =>{
+    const userDiv = document.createElement('div');
+    const userInfo = document.createElement('div');
+    userDiv.classList.add('user-details');
+    userInfo.classList.add('user-info');
+    
+    const name = document.createElement('h2');
+    name.textContent = result.firstName + ' ' + result.lastName;
+
+    const email = document.createElement('p');
+    email.textContent = 'Email: ' +result.email;
+
+    const gender = document.createElement('p');
+    gender.textContent = 'Gender: ' + result.gender;
+
+    const phone = document.createElement('p');
+    phone.textContent = 'Phone' + result.phone;
+
+    const location = document.createElement('p');
+    location.textContent = `Localización: ${result.location.street} - ${result.location.city } - 
+                            ${result.location.state} - ${result.location.country}`;
+    const img = document.createElement('img');
+    img.src = result.picture;
+
+    userInfo.appendChild(name);
+    userInfo.appendChild(email);
+    userInfo.appendChild(gender);
+    userInfo.appendChild(phone);
+    userInfo.appendChild(location);
+    
+    userDiv.appendChild(userInfo);
+    userDiv.appendChild(img);
+
+    container.appendChild(userDiv);
+}
+
+const showPosts = (result) =>{
+    const postsContainer = document.createElement('div');
+    postsContainer.classList.add('posts-container');
+    result.forEach(post => {
+        const postDiv = document.createElement('div');
+        postDiv.classList.add('post-bx');
+
+        
+        const title = document.createElement('h3');
+        title.textContent = post.text;
+
+        const img = document.createElement('img');
+        img.src = post.image;
+
+        const likes = document.createElement('p');
+        likes.textContent = 'Likes: ' + post.likes;
+
+        const tags = document.createElement('p');
+        tags.textContent = 'Tags: ' + post.tags;
+
+        const date = document.createElement('p');
+        date.textContent = 'Date: ' + post.publishDate;
+
+        postDiv.appendChild(title);
+        postDiv.appendChild(img);
+        postDiv.appendChild(likes);
+        postDiv.appendChild(tags);
+        postDiv.appendChild(date);
+
+        postsContainer.appendChild(postDiv);
+    });
+    container.appendChild(postsContainer);
+}
--- JS/asincronia/fetch/usersApp/script.js
+++ JS/asincronia/fetch/usersApp/script.js
@@ -1,25 +1,67 @@
-const options = {
+
+let delOptions = {
+    method: 'DELETE',
+    headers: {
+        'app-id': '6655bc199d926479175f28c2'
+    }
+};
+
+const getUserOptions = {
 	method: 'GET',
 	headers: {
 		'app-id': '6655bc199d926479175f28c2'
 	}
 };
+// const editOptions = {
+//     method: 'PUT',
+//     headers: {
+//         'app-id': '6655bc199d926479175f28c2',
+//         'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify(user)
+// };
 
 const getUsers = async () => {
     try {
-    	const response = await fetch('https://dummyapi.io/data/v1/user?limit=10', options);
+    	const response = await fetch('https://dummyapi.io/data/v1/user?limit=10', getUserOptions);
     	if(!response.ok)
             throw new Error("Error al obtener los datos");
         const result = await response.json();
-        showUsers(result);
+        console.log(result.data);
+        showUsers(result.data);
     } catch (error) {
     	console.error(error);
     }
 }
 
+const deleteUser = async (userId) => {
+    try {
+        const response = await fetch(`https://dummyapi.io/data/v1/user/${userId}`, delOptions);
+        if(!response.ok)
+            throw new Error("Error al eliminar los datos");
+        getUsers();
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+// const editUser = async (userId) => {
+//     try {
+//         const response = await fetch(`https://dummyapi.io/data/v1/user/${userId}`, editOptions);
+//         if(!response.ok)
+//             throw new Error("Error al editar los datos");
+//         getUsers();
+//         console.log(response);
+//     } catch (error) {
+//         console.error(error);
+//     }
+// }
+
 const showUsers = (result) => {
     const container = document.querySelector('#results');
-    result.data.forEach(user => {
+    const hiddenContainers = document.querySelectorAll('.hidden');
+    container.innerHTML = '';
+    result.forEach(user => {
         const userDiv = document.createElement('div');
         userDiv.classList.add('user');
 
@@ -32,20 +74,61 @@ const showUsers = (result) => {
         const btnCont = document.createElement('div');
         btnCont.classList.add('btn-container');
 
-        const edit = document.createElement('button');
-        edit.textContent = 'Edit';
-        btnCont.appendChild(edit);
+        const btnEdit = document.createElement('button');
+        btnEdit.textContent = 'Edit';
+        btnCont.appendChild(btnEdit);
 
-        const del = document.createElement('button');
-        del.textContent = 'Delete';
-        btnCont.appendChild(del);
-````````````````
+        const btnDel = document.createElement('button');
+        btnDel.textContent = 'Delete';
+        btnCont.appendChild(btnDel);
+        
         userDiv.appendChild(name);
         userDiv.appendChild(img);
         userDiv.appendChild(btnCont);
+        img.addEventListener('click', () => {
+            window.location.href = `details.html?id=${user.id}`;
+        });
 
         container.appendChild(userDiv);
+        btnDel.addEventListener('click', () => {
+            deleteUser(user.id);
+        });
+
+        btnEdit.addEventListener('click', () => {
+            showEditUser(user);
+            console.log(user);
+            hiddenContainers[0].classList.remove('hidden');
+        });
     });
 }
+
+showEditUser = (user) => {
+    const container = document.querySelector('#edit-form');
+    container.innerHTML = '';
+    
+    const title = document.createElement('input');
+    title.type = 'text';
+    title.placeholder = user.title;
+
+    const firstName = document.createElement('input');
+    firstName.type = 'text';
+    firstName.placeholder = user.firstName;
+
+    const lastName = document.createElement('input');
+    lastName.type = 'text';
+    lastName.placeholder = user.lastName;
+
+    const picture = document.createElement('input');
+    picture.type = 'text';
+    picture.placeholder = user.picture;
+
+    container.appendChild(title);
+    container.appendChild(firstName);
+    container.appendChild(lastName);
+    container.appendChild(picture);
+
+}
+
+
 
 document.addEventListener('DOMContentLoaded', getUsers);
